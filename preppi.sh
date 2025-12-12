266fbb354a9f53629feb0252f132330efed2ae7f
Gunars Frickauss <gunarsf@gmail.com>
2025-12-12 15:02:19 +0200

    Simplify package installation commands in preppi.sh

---
+++ preppi.sh
@@ -0,0 +1,5 @@
+wget https://info-beamer.com/jump/download/player/info-beamer-pi-2.2.0.60787b-bookworm-32.tar.gz
+apt-get update && apt-get dist-upgrade
+apt-get update && sudo apt-get dist-upgrade -y
+apt-get install ffmpeg libfreetype6 libevent-2.1-7 libgbm1 libegl-mesa0 libao4
+apt-get install libEGL*
