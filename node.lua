b3068c025194af33ce5fb60d1fd48802b27872aa
Gunars Frickauss <gunarsf@gmail.com>
2026-01-08 13:24:23 +0200

    Adjust window offsets and simplify load_next function
    Refactor window dimensions and update load_next logic.

--- node.lua
+++ node.lua
@@ -1,11 +1,8 @@
 local w=NATIVE_WIDTH
 local h=NATIVE_HEIGHT
-local BORDER = 0
-local mainwin_w = w - BORDER
-local mainwin_h = h - BORDER
-
-print(mainwin_w, mainwin_h)
-gl.setup(w, h)
+--Set window offset 
+local WIN_OFFSET_W = 10
+local WIN_OFFSET_H = 10
 
 local INTERVAL = 20
 -- enough time to load next image
@@ -16,11 +13,14 @@ local SWITCH_TIME = 2.0
 --BACKGROUND = resource.load_image('background.jpg')
 local pict = nil
 local video = nil
+local interval = INTERVAL
 
-assert(SWITCH_TIME + SWITCH_DELAY < INTERVAL,
+mainwin_w = w - WIN_OFFSET_W	
+mainwin_h = h - WIN_OFFSET_H
+gl.setup(w, h)
+
+assert(SWITCH_TIME + SWITCH_DELAY < interval,
     "INTERVAL must be longer than SWITCH_DELAY + SWITCHTIME")
-
-	local interval = INTERVAL
 
 util.file_watch("playlist.txt", function(content) 
 	playlist = {}
@@ -34,31 +34,36 @@ util.file_watch("playlist.txt", function(content)
 	pp(playlist)
 end)
 
-function get_next()
-	show_start = sys.now()
-	current_file_idx = current_file_idx + 1
-	if current_file_idx > #playlist then
-		current_file_idx = 1
-	end
-	local f = {}
-	for p in playlist[current_file_idx]:gmatch("([^,]+)") do
-		table.insert(f, p )
-	end
-	--next_file = playlist[current_file_idx]
-	local next_file = f[1]
-	interval = INTERVAL
-	if f[2] ~= nil then
-		local numval
-		numval = tonumber(f[2])
-		if numval and (SWITCH_TIME + SWITCH_DELAY < numval) then
-			interval = numval
-		end
-	end
-	print("Showing ..." .. next_file .. " interval: " .. interval)
-	return next_file
+function split(str, sep)
+    local result = {}
+    for part in string.gmatch(str, "([^" .. sep .. "]+)") do
+        table.insert(result, part)
+    end
+    return result
 end
 
-function load_next(file)
+function get_next()
+		interval = INTERVAL
+		show_start = sys.now()
+		local n,s
+		
+		current_file_idx = current_file_idx + 1
+		if current_file_idx > #playlist then
+			current_file_idx = 1
+		end
+		n = split(playlist[current_file_idx],",")
+		next_file = n[1]
+		if n[2] ~= nil then
+			s = tonumber(n[2])
+			if  s ~= nil and (SWITCH_TIME + SWITCH_DELAY < s) then
+				interval = s
+			end
+		end
+		print("Showing ..." .. next_file .. " for " .. interval .. "s")
+		return next_file
+end
+
+function load_next()
 	if string.upper(next_file):match(".*JPG") then
 		pict = resource.load_image(next_file)
 		if video then
@@ -69,37 +74,33 @@ function load_next(file)
 		video = util.videoplayer(next_file, {audio=true})
 		--video = util.videoplayer(playlist[current_video_idx], {audio=true, loop=true})
 		--video = resource.load_video(playlist[current_video_idx], true, true)
-		
 		if pict then
 			pict:dispose()
 			pict = nil
 		end
 	end
 end
---util.set_interval(interval, show_next)
-next_file = get_next()
--- Skip if comment
-while string.sub(next_file, 1, 1) == "#" do
-	next_file = get_next()
-end
-load_next(next_file)
+--util.set_interval(INTERVAL, show_next)
+load_next(get_next())
 
 function node.render()
-	--gl.clear(0, .2, 0, 1) -- green
+	if next_file == "" or next_file:sub(1, 1) == "#" then
+		load_next(get_next())
+		return
+	end
 	gl.clear(1, 1, 1, 1)
 	local delta = sys.now() - show_start - SWITCH_TIME
 	--print(delta .. " > " .. 1+delta/SWITCH_TIME)
-	--BACKGROUND:draw(5, 5, mainwin_w-BORDER, mainwin_h-BORDER)
+	--BACKGROUND:draw(5, 5, mainwin_w-7, mainwin_h-7)
 	if pict then
-		if delta > interval then
-			print("Delta: " .. delta)
+		if delta>interval then
 			load_next(get_next())
 		end
 		if delta > 0 then 
-			util.draw_correct(pict,BORDER, BORDER, mainwin_w-BORDER, mainwin_h-BORDER)
-		elseif delta < 0 then
+			util.draw_correct(pict, WIN_OFFSET_W, WIN_OFFSET_H, mainwin_w-WIN_OFFSET_W, mainwin_h-WIN_OFFSET_H)
+		elseif delta <0 then
 			local progress = delta / SWITCH_TIME
-			util.draw_correct(pict,BORDER, BORDER, mainwin_w-BORDER, mainwin_h-BORDER, 1+progress)
+			util.draw_correct(pict, WIN_OFFSET_W, WIN_OFFSET_H, mainwin_w-WIN_OFFSET_W, mainwin_h-WIN_OFFSET_H, 1+progress)
 		end
 	elseif video then
 		if not video:next() then
@@ -107,8 +108,7 @@ function node.render()
 			video=nil
 			load_next(get_next())
 		else
-			util.draw_correct(video,10, 10, mainwin_w-BORDER, mainwin_h-BORDER)
+			util.draw_correct(video,WIN_OFFSET_W, WIN_OFFSET_H, mainwin_w-WIN_OFFSET_W, mainwin_h-WIN_OFFSET_H)
 		end
 	end
-
 end
